5a2e1761e735968c5bcb390f8d51bad68e9c2a3e
RaduM84 <morarur@gmail.com>
2017-08-04 03:18:57 +0300

    Added "bonus_map" to tech_mapping.txt
    Added "bonus_map" section to tech_mapping.txt that allows for Research Bonuses to be specified for countries

--- Vic2ToHoI4/Source/HOI4World/HoI4Country.cpp
+++ Vic2ToHoI4/Source/HOI4World/HoI4Country.cpp
@@ -1087,6 +1087,15 @@ void HoI4Country::setTechnology(string tech, int level)
 	}
 }
 
+void HoI4Country::setResearchBonus(string tech, int bonus)
+{
+	map<string, int>::iterator researchBonusEntry = researchBonuses.find(tech);
+	if ((researchBonusEntry == researchBonuses.end()) || (researchBonusEntry->second < bonus))
+	{
+		researchBonuses[tech] = bonus;
+	}
+}
+
 
 void HoI4Country::calculateIndustry()
 {
@@ -1328,6 +1337,15 @@ void HoI4Country::outputTechnology(ofstream& output) const
 		output << tech.first << " = 1\n";
 	}
 	output << "}\n";
+}
+
+void HoI4Country::outputResearchBonuses(ofstream& output) const
+{	
+	output << "# Research Bonuses\n";
+	for (auto researchBonus : researchBonuses)
+	{		
+		output << "add_tech_bonus = { bonus = " << float(researchBonus.second/100) << " uses = 1 category = " << researchBonus.first << "}\n";
+	}
 }
 
 
