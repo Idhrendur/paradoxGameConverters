ce50c864768647d5513792393cf7c88d92979ea7
Johuotar <jmh@dnainternet.net>
2018-02-19 20:47:27 +0200

    Revert "2 monarch ideas"
    This reverts commit 618c7e20970a0b07be4325cacce766702d64b7bb.

--- Vic2ToHoI4/Source/HOI4World/HoI4FocusTree.cpp
+++ Vic2ToHoI4/Source/HOI4World/HoI4FocusTree.cpp
@@ -2587,7 +2587,7 @@ void HoI4FocusTree::addAbsolutistEmpireNationalFocuses(shared_ptr<HoI4Country> H
 	newFocus->aiWillDo += "			factor = 10\n";
 	newFocus->aiWillDo += "			modifier = {\n";
 	newFocus->aiWillDo += "			}";
-	newFocus->completionReward += "			add_ideas = colonial_divisions";
+	newFocus->completionReward += "			add_ideas = militarism_focus";
 	focuses.push_back(newFocus);
 
 	//establish protectorate
@@ -2992,7 +2992,7 @@ void HoI4FocusTree::addAbsolutistEmpireNationalFocuses(shared_ptr<HoI4Country> H
 	newFocus->aiWillDo += "			factor = 10\n";
 	newFocus->aiWillDo += "			modifier = {\n";
 	newFocus->aiWillDo += "			}";
-	newFocus->completionReward += "			add_ideas = nationalistic_spirit";
+	newFocus->completionReward += "			add_ideas = paramilitarism_focus";
 	focuses.push_back(newFocus);
 
 	//ANNEX
