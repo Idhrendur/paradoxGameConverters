7ba28d5e7188d325e4008a245543838b4b8258d2
Idhrendur <idhrendur@hotmail.com>
2018-09-02 10:17:27 -0700

    Add tests to verify the rest of getOutputName()

--- Vic2ToHoI4Tests/Vic2ToHoI4ConverterTests.cpp
+++ Vic2ToHoI4Tests/Vic2ToHoI4ConverterTests.cpp
@@ -16,11 +16,53 @@ namespace ConverterMainTests
 TEST_CLASS(getOutputNameTests)
 {
 	public:
-		TEST_METHOD(TypeNotSetWhenNotGivenInput)
+		TEST_METHOD(filenameEmptyWhenGivenNoInput)
+		{
+			std::string inputName = "";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string(""), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameCorrectWhenSimplyAName)
+		{
+			std::string inputName = "hoi4.v2";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string("hoi4"), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameExtractedWithWindowsPath)
+		{
+			std::string inputName = "C:\\Users\\Cosmostarr\\Documents\\Paradox Interactive\\Victoria II\\save games\\hoi4.v2";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string("hoi4"), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameExtractedWithLinuxPath)
+		{
+			std::string inputName = "/Users/Cosmostarr/Documents/Paradox Interactive/Victoria II/save games/hoi4.v2";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string("hoi4"), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameExtractedWithMixedPathEndingLinuxStyle)
 		{
 			std::string inputName = "C:\\Users\\Cosmostarr\\Documents\\Paradox Interactive\\Victoria II\\save games/hoi4.v2";
 			getOutputName(inputName);
-			Assert::AreEqual(std::string("hoi4.v2"), theConfiguration.getOutputName());
+			Assert::AreEqual(std::string("hoi4"), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameExtractedWithMixedPathEndingWindowsStyle)
+		{
+			std::string inputName = "/Users/Cosmostarr/Documents/Paradox Interactive/Victoria II/save games\\hoi4.v2";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string("hoi4"), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameHasDashesReplaced)
+		{
+			std::string inputName = "hoi4-something.v2";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string("hoi4_something"), theConfiguration.getOutputName());
+		}
+		TEST_METHOD(filenameHasSpacesReplaced)
+		{
+			std::string inputName = "hoi4 something.v2";
+			getOutputName(inputName);
+			Assert::AreEqual(std::string("hoi4_something"), theConfiguration.getOutputName());
 		}
 };
 
