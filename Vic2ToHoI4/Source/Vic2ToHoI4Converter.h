85c41324ccc106764a35aafb76bbde0d97066136
Idhrendur <idhrendur@hotmail.com>
2018-12-01 19:32:27 -0800

    Switch to google tests (#695)
    * Add unit tests for Vic2::Agreement
    
    * Move military templates into their own class
    
    * Add tests for the military mappings
    
    * Fix merge error
    
    * Add capability to translate from Win1252 to UTF-8
    
    * Add capability to convert into win1252
    
    * Convert Vic2 localisations from WIn1252 instead of ISO 8859-15.
    
    * Fixes for Linux version.
    
    * Eliminate a crash when converting armies in capitalless nations.
    
    * Properly include header on Linux.
    
    * German province mapping updates
    
    * Allow the desired ideologies to be selected (requires manually editing
    configuration file).
    
    * Add google test framework
    
    * Fix a build bug with HoI4 agreements that somehow never came up before.
    
    * Split the converter main function into its own file.
    
    * Set the C++ version correctly for google tests
    
    * Adjust files linked to google tests
    
    * Clean up first set of google tests.
    
    * Add existing HoI4 world test to Google tests
    
    * Make overall tests more clear in GTest
    
    * Fix a HoI4 regiment bug found by GTest
    
    * Add existing Vic2World tests to GTest
    
    * Remove old test suite
    
    * Replace previous test with GTest

--- Vic2ToHoI4/Source/Vic2ToHoI4Converter.h
+++ Vic2ToHoI4/Source/Vic2ToHoI4Converter.h
@@ -21,44 +21,8 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 
 
-#ifndef HOI4_REGIMENT_H
-#define HOI4_REGIMENT_H
-
-
-
-#include "newParser.h"
-#include <istream>
-#include <ostream>
 #include <string>
 
 
 
-namespace HoI4
-{
-
-class RegimentType: commonItems::parser
-{
-	public:
-		explicit RegimentType(const std::string& _type, std::istream& theStream);
-		RegimentType(const RegimentType&) = default;
-
-		const std::string getType() const { return type; }
-
-		friend std::ostream& operator << (std::ostream& out, const RegimentType& regiment);
-
-	private:
-		RegimentType& operator=(const RegimentType&) = delete;
-
-		std::string	type;
-		int x = 0;
-		int y = 0;
-};
-
-
-std::ostream& operator << (std::ostream& out, const RegimentType& regiment);
-
-}
-
-
-
-#endif // HOI4_REGIMENT_H
+void ConvertV2ToHoI4(const std::string& V2SaveFileName);
--- Vic2ToHoI4/Source/main.cpp
+++ Vic2ToHoI4/Source/main.cpp
@@ -21,44 +21,41 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 
 
-#ifndef HOI4_REGIMENT_H
-#define HOI4_REGIMENT_H
+#include "Log.h"
+#include "Vic2ToHoI4Converter.h"
+#include "OSCompatibilityLayer.h"
 
 
 
-#include "newParser.h"
-#include <istream>
-#include <ostream>
-#include <string>
-
-
-
-namespace HoI4
+int main(const int argc, const char* argv[])
 {
+	try
+	{
+		LOG(LogLevel::Info) << "Converter version 0.2H";
+		LOG(LogLevel::Info) << "Built on " << __DATE__ << " at " << __TIME__;
+		LOG(LogLevel::Info) << "Current directory is " << Utils::getCurrentDirectory();
 
-class RegimentType: commonItems::parser
-{
-	public:
-		explicit RegimentType(const std::string& _type, std::istream& theStream);
-		RegimentType(const RegimentType&) = default;
+		const char* const defaultV2SaveFileName = "input.v2";
+		std::string V2SaveFileName;
+		if (argc >= 2)
+		{
+			V2SaveFileName = argv[1];
+			LOG(LogLevel::Info) << "Using input file " << V2SaveFileName;
+		}
+		else
+		{
+			V2SaveFileName = defaultV2SaveFileName;
+			LOG(LogLevel::Info) << "No input file given, defaulting to " << defaultV2SaveFileName;
+		}
 
-		const std::string getType() const { return type; }
+		ConvertV2ToHoI4(V2SaveFileName);
 
-		friend std::ostream& operator << (std::ostream& out, const RegimentType& regiment);
+		return 0;
+	}
 
-	private:
-		RegimentType& operator=(const RegimentType&) = delete;
-
-		std::string	type;
-		int x = 0;
-		int y = 0;
-};
-
-
-std::ostream& operator << (std::ostream& out, const RegimentType& regiment);
-
+	catch (const std::exception& e)
+	{
+		LOG(LogLevel::Error) << e.what();
+		return -1;
+	}
 }
-
-
-
-#endif // HOI4_REGIMENT_H
